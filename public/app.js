66f4f99e85f88ef384adfa3d65bbd5ad7e90ca77
nickhillwd <nickhillwd@gmail.com>
2016-02-01 17:10:52 +0000

    add function running and only adding one quote

--- public/app.js
+++ public/app.js
@@ -3,7 +3,6 @@ var allQuotes = [];
 var Quote = function(text, author){
   this.text = text;
   this.author = author;
-  allQuotes.push(this);
 }
 
 var jayQuote = new Quote("Visual Basic is the way forward, I don't know why we are doing Javascript.", "Jay Chetty");
@@ -14,18 +13,54 @@ var valQuote = new Quote("Scaffold is nothing but a fiery hell.", "Valerie");
 console.log("All Quotes Array:", allQuotes);
 
 window.onload = function(){
-
-  for (var i = 0; i < allQuotes.length; i++) {
+  var renderQuotes = function(){
     var quote = document.getElementById("quote");
-    var quoteArticle = document.createElement("article");
-    var blockQuote = document.createElement("blockquote");
-    var cite = document.createElement("cite");
+    quote.innerHTML = "";
+    for (var i = 0; i < allQuotes.length; i++) {
+      var quoteArticle = document.createElement("article");
+      var blockQuote = document.createElement("blockquote");
+      var cite = document.createElement("cite");
 
-    cite.innerText = allQuotes[i].author;
-    blockQuote.innerText = this.allQuotes[i].text;
-    quoteArticle.classList.add("quote");
-    blockQuote.appendChild(cite);
-    quoteArticle.appendChild(blockQuote);
-    quote.appendChild(quoteArticle);
-  };
+      cite.innerText = allQuotes[i].author;
+      blockQuote.innerText = this.allQuotes[i].text;
+      quoteArticle.classList.add("quote");
+      blockQuote.appendChild(cite);
+      quoteArticle.appendChild(blockQuote);
+      quote.appendChild(quoteArticle);
+    };
+
+  }
+
+  var handleClick = function(event){
+    console.log('I have been clicked')
+    event.preventDefault();
+    var quoteInputText = document.getElementById("newquote").value;
+    console.log("Quote Text Input:", quoteInputText);
+    var authorInputText = document.getElementById("newquoteauthor").value;
+    console.log("Author Text Input:", authorInputText);
+
+    var newQuote = new Quote(quoteInputText, authorInputText);
+    console.log('newquote', newQuote);
+    allQuotes.push(newQuote);
+    console.log(allQuotes);
+    renderQuotes();
+
+  }
+
+  renderQuotes();
+
+  var button = document.getElementById("addbutton");
+  button.onclick = handleClick;
 }
+
+
+
+
+
+
+
+
+
+
+
+
